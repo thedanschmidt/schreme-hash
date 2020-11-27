24903bbd74f7eae36427e7cc867332d740fd0a0c
Dan Schmidt <danielschmidt24@gmail.com>
2020-11-27 13:13:07 -0800

    initial sketches of design

--- inc/schreme_hash.h
+++ inc/schreme_hash.h
@@ -1,3 +1,52 @@
 #pragma once
 
+/*
+ *  
+ */
 
+enum schreme_table_types =
+{
+  FIXED_KEY = 0,
+  VAR_KEY
+};
+
+enum schreme_table_err =
+{
+  NO_ERROR = 0,
+  OUT_OF_MEMORY,
+  INTERNAL_ERROR
+};
+
+/*
+ * key value structs
+ */
+struct schreme_tbl
+{
+  uint32_t key;
+  uint32_t val_offset;
+};
+
+struct schreme_tbl_val
+{
+  uint16_t size = 0;
+  const uint8_t* data = NULL;
+};
+
+/*
+ * simple linear time lookup tables
+ */
+struct schreme_fkey_lookup_table
+{
+  schreme_tbl table;
+  uint8_t* val_table;
+};
+
+schreme_tbl_err schreme_tbl_insert_fkey(uint32_t key, schreme_tbl_val* val);
+schreme_tbl_err schreme_tbl_lookup_fkey(uint32_t key, schreme_tbl_val* val);
+schreme_tbl_err schreme_tbl_erase_fkey(uint32_t key, schreme_tbl_val* val);
+
+struct schreme_vkey_lookup_table
+{
+  uint8_t* key_table;
+  schreme_fkey_lookup_table* indexed_tbl;
+};
--- src/schreme_hash.c
+++ src/schreme_hash.c
@@ -1,1 +1,16 @@
 #include "schreme_hash.h"
+
+schreme_tbl_err schreme_tbl_insert_fkey(uint32_t key, schreme_tbl_val* val)
+{
+  return 0;
+}
+
+schreme_tbl_err schreme_tbl_lookup_fkey(uint32_t key, schreme_tbl_val* val)
+{
+  return 0;
+}
+
+schreme_tbl_err schreme_tbl_erase_fkey(uint32_t key, schreme_tbl_val* val)
+{
+  return 0;
+}
